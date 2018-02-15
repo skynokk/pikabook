a2f0d9f5c668b85f806e9e1420eeec0f6cf50485
Lorenzo Logos <lorenzo.logos@ynov.com>
2018-02-15 16:38:56 +0100

    rangement

---
+++ code/index.js
@@ -0,0 +1,5 @@
+
+// Alerte notifiant l'utilisateur que ce site n'est pas un réel site de commerce
+function disclaimer() {
+  alert("Bonjour,\nVous vous trouvez actuellement sur un site test réalisé dans le cadre de nos études.\nCe site n'est donc pas fonctionnel, n'entrez en aucun cas vos informations banquaires.\nMerci de votre compréhension");
+}
---
+++ code/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pik a Book</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../Images/Pikachu_vect.png" />
+    <link rel="stylesheet" href="pikabook.css">
+    <script src="index.js" charset="utf-8"></script>
+  </head>
+  <body onload="disclaimer()">
+<header>
+
+<?php include 'header.inc.php'; ?>
+
+</header>
+
+<div class="rewiew">
+<h1>Bienvenue sur Pik a book</h1>
+<p>Notre bibliothèque en ligne vous permettra d'acheter au format pdf tout types de livres séparé par catégories (Romans, Manga, Comics, Bande-dessinées,Encyclopédie,etc...) et par genres (Action, Aventure, Science-Fiction, etc...).</p>
+<p>Vous pourrez aussi découvrir nos coups de coeur via la page "A la une", les livres les plus populaires via la page "Best-sellers" ainsi que les derniers livres ajoutés.</p>
+<p>Il vous faudra donc créer un compte afin de pouvoir acheter un ou plusieurs livres et profiter de notre bibliothèque.</p>
+<p>Merci pour votre visite et bonne lecture !</p>
+</div>
+
+  </body>
+</html>
