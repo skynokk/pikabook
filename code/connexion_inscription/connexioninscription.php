31de95f2b7e0d1900c42e275c7a8f8e37989876a
unknown <stacy.perales@ynov.com>
2018-02-19 16:19:12 +0100

    stacy

---
+++ code/connexion_inscription/connexioninscription.php
@@ -0,0 +1,21 @@
+<?php include '../fichier_inc/header.inc.php'; ?>
+<div class="connexion">
+  <input type="button" name="connect" value="Connexion">
+  
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include '../fichier_inc/footer.inc.php'; ?>
