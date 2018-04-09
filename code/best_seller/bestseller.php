b261005dfb27062aa669616d292363f7c7710891
Nicolas Gerard <nicolas.gerard@ynov.com>
2018-04-09 12:30:16 +0200

    Nouvellepagebestseller

---
+++ code/best_seller/bestseller.php
@@ -0,0 +1,7 @@
+<?php include '../fichier_inc/header.inc.php'; ?>
+
+<div class="divPageBestSeller">
+  	<div class="divBestSeller"><img class="imageBestSeller" src="../../Images/livre1.jpg" ></div>
+</div>
+
+<?php include '../fichier_inc/footer.inc.php'; ?>
