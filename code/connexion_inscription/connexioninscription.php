ee69b8ead4936b84af8eb2cccacd5192435babb6
Lau1234 <laura.graca@ynov.com>
2018-04-09 13:02:11 +0200

    Ajout de la connexion à la BDD et debut php sur la page d'inscription

--- code/connexion_inscription/connexioninscription.php
+++ code/connexion_inscription/connexioninscription.php
@@ -1,49 +1,68 @@
-<?php include '../fichier_inc/header.inc.php'; ?>
+<?php 
+  include '../fichier_inc/connexionBDD.inc.php';
+  include '../fichier_inc/header.inc.php';      
+?>
 <br><br>
 <div class="connexion">
+
   <p class="connect">Connexion</p>
   <br><br>
-      <div class="Noms">
-      <label for="nom" class="label">Pseudo:</label>
-      <input type="text" id="nom" placeholder="Pseudo" />
+  <form method="POST" action=<?php echo $_SERVER["PHP_SELF"]; ?>>
+    <div class="Noms">
+     <label for="nom" class="label">Pseudo:</label>
+      <input type="text" id="pseudoconn" placeholder="Pseudo" />
     </div>
-  <div class="Mpd">
-    <label for="mdp">Mot de passe: </label>
-    <input type="password" id="mdp" name="mdp"/>
-  </div>
-  <input type="submit" name="valider" value="Valider">
-</div>
+    <div class="Mpd">
+      <label for="mdp">Mot de passe: </label>
+      <input type="password" id="mdp" name="mdpconn"/>
+    </div>
+     <input type="submit" name="valider" value="Valider">
+    </div>
+  </form>
 
 <div class="Inscription">
-  <p class="incript">Pas encore inscrit? Rejoignez-nous!</p>
-  <div class="Noms">
-  <label for="nom" class="label">Nom:</label>
-  <input type="text" id="nom" placeholder="Nom" />
-  </div>
-  <div class="Noms">
-  <label for="nom" class="label">Prénom:</label>
-  <input type="text" id="nom" placeholder="Prénom" />
-  </div>
-  <div class="Adresse">
-  <label for="nom" class="label">Adresse mail:</label>
-  <input type="text" id="nom" placeholder="pikabook@gmail.com"/>
-  </div>
-  <div class="Pseudo">
-  <label for="nom" class="label">Pseudo:</label>
-  <input type="text" id="nom" placeholder="Pseudo"/>
-  </div>
-  <div class="Mpd">
-    <label for="mdp">Mot de passe: </label>
-    <input type="password" id="mdp" name="mdp"/>
-  </div>
-  <div class="Mpd">
-    <label for="mdp">Confirmation: </label>
-    <input type="password" id="mdp" name="mdp"/>
-  </div>
-  <input type="submit" name="inscrire" value="S'inscrire">
+  <form method="POST" action=<?php echo $_SERVER["PHP_SELF"]; ?>>
+    <p class="incript">Pas encore inscrit ? Rejoignez-nous!</p>
+    <div class="Noms">
+      <label for="nom" class="label">Nom:</label>
+      <input type="text" id="nom" name="nom" placeholder="Nom" />
+    </div>
+    <div class="Noms">
+      <label for="prenom" class="label">Prénom:</label>
+      <input type="text" id="prenom" name="prenom" placeholder="Prénom" />
+    </div>
+    <div class="Adresse">
+      <label for="email" class="label">Adresse mail:</label>
+      <input type="text" id="email" name="email" placeholder="pikabook@gmail.com"/>
+    </div>
+    <div class="Pseudo">
+      <label for="pseudoconn" class="label">Pseudo:</label>
+      <input type="text" id="pseudoinscr" name="pseudoinscr" placeholder="Pseudo"/>
+    </div>
+    <div class="Mpd">
+      <label for="mdp">Mot de passe: </label>
+      <input type="password" id="mdpinscr" name="mdpinscr"/>
+    </div>
+    <div class="Mpd">
+      <label for="confirmdp">Confirmation: </label>
+      <input type="password" id="confirmdp" name="confirmdp"/>
+    </div>
+    <input type="submit" name="inscrire" value="S'inscrire">
+  </form>
 </div>
 
+<?php
+  if (isset($_POST['inscrire'])){ /*s'active uniquement quand tu appuies su r le bouton s'inscrire*/ 
+    if (isset($_POST['nom']) && isset($_POST["prenom"]) && isset($_POST["email"]) && isset($_POST["pseudoinscr"]) && isset($_POST["mdpinscr"]) && isset($_POST["confirmdp"])  ) /* on vérifie que tous les critères sont remplis*/{
+      echo "<h1><font color='red'>Entre commande</font></h1>";
+    }
+    else {
+      echo "<h1><font color='white'>Ca a pas marché</font></h1>";
+    }
+  }
+  
 
+?>
 
 
 
