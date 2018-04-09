bed42a68ba9c859d85c5fee92ae40a6a30e253ad
Nicolas Gerard <nicolas.gerard@ynov.com>
2018-04-09 12:49:59 +0200

    ModifBestSeller

--- code/best_seller/bestseller.php
+++ code/best_seller/bestseller.php
@@ -1,7 +1,10 @@
 <?php include '../fichier_inc/header.inc.php'; ?>
 
 <div class="divPageBestSeller">
-  	<div class="divBestSeller"><img class="imageBestSeller" src="../../Images/livre1.jpg" ></div>
+  <div class="ligne">
+      <div class="divBestSeller"><img class="imageBestSeller" src="../../Images/livre1.jpg" > </div>
+      <p class="texteBestSeller">Test</p>
+  </div>
 </div>
 
 <?php include '../fichier_inc/footer.inc.php'; ?>
