4c422e071d8235937c3821aafe5237b2e2d2e25e
skynokk <jean.ferro@ynov.com>
2018-04-12 14:46:55 +0200

    modification_page

---
+++ code/derniers_ajouts/derniersajouts.php
@@ -0,0 +1,5 @@
+<?php include '../fichier_inc/header.inc.php'; ?>
+
+
+
+<?php include '../fichier_inc/footer.inc.php'; ?>
--- code/fichier_inc/header.inc.php
+++ code/fichier_inc/header.inc.php
@@ -9,9 +9,9 @@ echo "<!DOCTYPE html>
 
 	<body>
 	<header>
-		<div >
+		<div>
+		<input type=\"button\" class=\"co\" value=\"Inscription\" onclick=\"window.location.href='../connexion_inscription/connexioninscription.php';\" />
 			<h1><img class=\"logo\" src=\"../../Images/logo4_1.png\" alt=\"\"/></h1>
-
 		     <div id=\"searchbar\">
 		        <input class=\"champ\" type=\"text\" placeholder=\"Search...\"/>
 		        <input class=\"bouton\" type=\"button\" value=\"&#128269; \" />
@@ -21,9 +21,10 @@ echo "<!DOCTYPE html>
 				<div class=\"cadre\">
 					<nav>
 						<li><a class=\"navigator\" href=\"../index.php\">Présentation</a></li>
+						<li><a class=\"navigator\" href=\"../a_la_une/alaune.php\">A la Une</a></li>
 						<li><a class=\"navigator\" href=\"../page_livre/pagelivre.php\">Livres</a></li>
 						<li><a class=\"navigator\" href=\"../best_seller/bestseller.php\">Les Best-Sellers</a></li>
-						<li><a class=\"navigator\" href=\"#\">Les derniers ajouts</a></li>
+						<li><a class=\"navigator\" href=\"../derniers_ajouts/derniersajouts.php\">Les derniers ajouts</a></li>
 						<li><a class=\"navigator\" href=\"../user/utilisateur.php\">Mon compte</a></li>
 					</nav>
 		</div>
