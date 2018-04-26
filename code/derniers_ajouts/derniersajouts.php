36008bd3aed70d4b7aa213e76e760726a4e05c70
unknown <stacy.perales@ynov.com>
2018-04-26 15:28:02 +0200

    dernierajout

--- code/derniers_ajouts/derniersajouts.php
+++ code/derniers_ajouts/derniersajouts.php
@@ -4,12 +4,11 @@
     <div class="divNouv"><p class="titreDiv">Derniers ajouts</p></div><br><br>
 
     <div class="infoLivre">
-
       <div class="resumAjout">
-        <p>Résumé:  <br/>
+        <p class="resum">Résumé:  <br/>
           <br/>
           Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.</br>
-          Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla</br>
+          Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla
           Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum,</br>
           et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
         </p>
