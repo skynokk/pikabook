fda0c1f873d359d1285a900215fe05b6066d9e4e
skynokk <jean.ferro@ynov.com>
2018-04-26 16:30:36 +0200

    bdd

--- code/fichier_inc/header.inc.php
+++ code/fichier_inc/header.inc.php
@@ -12,7 +12,7 @@ echo "<!DOCTYPE html>
 	<body>
 	<header>
 
-	<input type=\"button\" class=\"co\" value=\"Inscription\" onclick=\"window.location.href='../connexion_inscription/connexioninscription.php';\" />
+	<input type=\"button\" class=\"co\" value=\"Inscription\" onclick=\"window.location.href='../connexion_inscription/connexioninscription';\" />
 		<form method=\"POST\" action=\"\" id=\"form\">
 				<input type=\"text\" id=\"pseudo\" placeholder=\"Pseudo\" />
 				<input type=\"password\" id=\"mdp\" name=\"mdp\" placeholder=\"Mot de Passe\"/>
@@ -21,7 +21,7 @@ echo "<!DOCTYPE html>
 
 	<br/><br/>
 	<a href=\"../\"> <img src=\"../../Images/petit-panier.png\" class=\"panier\" alt=\"panier\"></a>
-	<a href=\"../vos_commandes/voscommandes.php\"><button type=\"button\" class=\"commande\" name=\"button\">Mes commandes</button></a>
+	<a href=\"../vos_commandes/voscommandes\"><button type=\"button\" class=\"commande\" name=\"button\">Mes commandes</button></a>
 
 
 			<h1><img class=\"logo\" src=\"../../Images/logo4_1.png\" alt=\"\"/></h1>
@@ -34,12 +34,12 @@ echo "<!DOCTYPE html>
 		    <div class=\"page\">
 				<div class=\"cadre\">
 					<nav>
-						<li><a class=\"navigator\" href=\"../index.php\">Présentation</a></li>
-						<li><a class=\"navigator\" href=\"../a_la_une/alaune.php\">A la Une</a></li>
-						<li><a class=\"navigator\" href=\"../page_livre/pagelivre.php\">Livres</a></li>
-						<li><a class=\"navigator\" href=\"../best_seller/bestseller.php\">Les Best-Sellers</a></li>
-						<li><a class=\"navigator\" href=\"../derniers_ajouts/derniersajouts.php\">Les derniers ajouts</a></li>
-						<li><a class=\"navigator\" href=\"../user/utilisateur.php\">Mon compte</a></li>
+						<li><a class=\"navigator\" href=\"../index\">Présentation</a></li>
+						<li><a class=\"navigator\" href=\"../a_la_une/alaune\">A la Une</a></li>
+						<li><a class=\"navigator\" href=\"../page_livre/pagelivre\">Livres</a></li>
+						<li><a class=\"navigator\" href=\"../best_seller/bestseller\">Les Best-Sellers</a></li>
+						<li><a class=\"navigator\" href=\"../derniers_ajouts/derniersajouts\">Les derniers ajouts</a></li>
+						<li><a class=\"navigator\" href=\"../user/utilisateur\">Mon compte</a></li>
 					</nav>
 		</div>
 	</header>";
--- code/index.php
+++ code/index.php
@@ -4,13 +4,13 @@
 		<meta charset="utf-8">
 	    <title>Pika Book</title>
 	    <link rel="stylesheet" href="pikabook.css">
-			<script type="text/javascript" src="index.js"></script>
+			<script type="text/javascript" src="jsindex.js"></script>
 	</head>
 
 	<body onload="disclaimer()">
 	<header>
 		<div>
-			<input type="button" class="co" value="Inscription" onclick="window.location.href='connexion_inscription/connexioninscription.php';" />
+			<input type="button" class="co" value="Inscription" onclick="window.location.href='connexion_inscription/connexioninscription';" />
 				<form method="POST" action="" id="form">
 						<input type="text" id="pseudo" placeholder="Pseudo" />
 						<input type="password" id="mdp" name="mdp" placeholder="Mot de Passe"/>
@@ -19,7 +19,7 @@
 			<br/><br/>
 
 			<a href="../"> <img src="../Images/petit-panier.png" class="panier" alt="panier"></a>
-			<a href="../vos_commandes/voscommandes.php"><button type="button" class="commande" name="button">Mes commandes</button></a>
+			<a href="vos_commandes/voscommandes"><button type="button" class="commande" name="button">Mes commandes</button></a>
 
 
 			<h1><img class="logo" src="../Images/logo4_1.png" alt=""/></h1>
@@ -33,11 +33,11 @@
 				<div class="cadre">
 					<nav>
 						<li><a class="navigator" href="#">Présentation</a></li>
-						<li><a class="navigator" href="a_la_une/alaune.php">A la Une</a></li>
-						<li><a class="navigator" href="page_livre/pagelivre.php">Livres</a></li>
-						<li><a class="navigator" href="best_seller/bestseller.php">Les Best-Sellers</a></li>
-						<li><a class="navigator" href="derniers_ajouts/derniersajouts.php">Les derniers ajouts</a></li>
-						<li><a class="navigator" href="user/utilisateur.php">Mon compte</a></li>
+						<li><a class="navigator" href="a_la_une/alaune">A la Une</a></li>
+						<li><a class="navigator" href="page_livre/pagelivre">Livres</a></li>
+						<li><a class="navigator" href="best_seller/bestseller">Les Best-Sellers</a></li>
+						<li><a class="navigator" href="derniers_ajouts/derniersajouts">Les derniers ajouts</a></li>
+						<li><a class="navigator" href="user/utilisateur">Mon compte</a></li>
 					</nav>
 		</div>
 	</header>
