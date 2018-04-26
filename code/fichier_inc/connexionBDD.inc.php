9e6240b3040f564be47d56f035eeab554d9a9b32
skynokk <jean.ferro@ynov.com>
2018-04-26 15:23:22 +0200

    bdd

--- code/fichier_inc/connexionBDD.inc.php
+++ code/fichier_inc/connexionBDD.inc.php
@@ -1,4 +1,4 @@
 <?php 
-	$pdo= new PDO ('mysql:host=localhost;dbname=pikabook','root','');
+	$pdo= new PDO ('mysql:host=localhost;dbname=pikabook_6v1l','pikabook_6v1l','pikabookadmin');
 	array (PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION);
 ?>
