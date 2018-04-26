85d1538859dc5813ccd1e4ad4c29f82f13dd49c7
unknown <stacy.perales@ynov.com>
2018-04-26 16:14:00 +0200

    dernierajout

---
+++ code/panier/panier.php
@@ -0,0 +1,11 @@
+<?php include '../fichier_inc/header.inc.php'; ?>
+
+<div class="pagePanier">
+  <div class="">
+    <h1>Votre panier</h1>
+  </div>
+</div>
+
+
+
+<?php include '../fichier_inc/footer.inc.php'; ?>
