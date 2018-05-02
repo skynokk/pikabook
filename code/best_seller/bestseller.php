f5d93374327138eaf10e653bd9a3819523034b4a
unknown <stacy.perales@ynov.com>
2018-05-02 16:32:25 +0200

    bestseller

--- code/best_seller/bestseller.php
+++ code/best_seller/bestseller.php
@@ -3,11 +3,16 @@ include '../fichier_inc/header.inc.php';
 ?>
 
 <div class="divPageBestSeller">
-  
+
   <div class="ligne">
       <div class="divBestSeller"><img class="imageBestSeller" src="../Images/livre1.jpg" > </div>
-      <div class="texteBestSeller"> <h2>ESCLAVE DE DAECH</h2> </div>
+      <div class="texteBestSeller"> <h2>ESCLAVE DE DAECH</h2> </div><br><br><br>
+      <div class="resumBest"><p>Le jour de ses onze ans, Harry POTTER, un orphelin élevé par un oncle et une tante qui le détenstent, voit son
+      existence boulversée. Un géant vient le chercher pour l'emmener à Poudlard, une école de sorcellerie! Voler, en balai,
+    jeter des sorts, combattre les trolls: Harry se révèle être un sorcier doué.
+  Mais quel est le mystère qui l'entoure? Et qui est l'effroyable V..., le mage dont personne n'ose prononcer le nom?</p>
   </div>
+</div>
 
   <div class="break"></div>
 
