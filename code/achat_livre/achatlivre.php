27c02b58c31d78cab96abed9f98d088465a6065a
Oznerologos <lorenzo.logos@ynov.com>
2018-05-02 19:28:43 +0200

    maj back

--- code/achat_livre/achatlivre.php
+++ code/achat_livre/achatlivre.php
@@ -1,57 +1,57 @@
 <?php include '../fichier_inc/header.inc.php'; ?>
 
-    <div id="divAchatLivre1">
-      <p id="pAchatLivre">
-        Resume :
-        <br/>
-        <br/>
-Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.</br>
- Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla</br>
- ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, </br>
- mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié.</br>
-  Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum,</br>
-   et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
-      </p>
-    </div>
+<?php
 
-    <div id="divAchatLivre2">
-    <img id="imgAchatLivre1" src="../Images/placeholder.png" alt="placeholder">
-    </div>
-<br/>
+  if (isset($_GET["produitID"])) {
+    $produitID = $_GET["produitID"];
+    $result = $pdo->query("SELECT * FROM livre WHERE LivreISBN='$produitID'");
+    $infoLivre = $result->fetch(PDO::FETCH_ASSOC);
 
-<img id="imgvote" src="../Images/vote.png" alt="vote">
+    // Affichage du genre
+    $genre= $infoLivre["GenreID"];
+    $resultGenre = $pdo->query("SELECT GenreNom FROM genre WHERE GenreID='$genre'");
+    $genreNom = $resultGenre->fetch(PDO::FETCH_ASSOC);
+    //
+    // Affichage de l'auteur
+    $auteur= $infoLivre["AuteurID"];
+    $resultAuteur = $pdo->query("SELECT AuteurNom FROM auteur WHERE AuteurID='$auteur'");
+    $auteurNom = $resultAuteur->fetch(PDO::FETCH_ASSOC);
+    //
+    // Affichage de l'editeur
+    $editeur= $infoLivre["EditID"];
+    $resultEdit = $pdo->query("SELECT EditNom FROM editeur WHERE EditID='$editeur'");
+    $editeurNom = $resultEdit->fetch(PDO::FETCH_ASSOC);
+    //
 
-<div id="prixal">
-  [Prix €]
-</div>
 
-<button id="buttonAchatLivre" type="button" name="button">Ajouter au panier <img id="imgAchatLivre2" src="../Images/caddie.png" alt=""> </button>
+    echo '
+    <div>
+      <div id="divAchatLivre1">
+        <p id="pAchatLivre">
+          <h3>Description du Produit :</h3>
+          Auteur : '.$auteurNom["AuteurNom"].' <br/>
+          Editeur : '.$editeurNom["EditNom"].' <br/>
+          Genre : '.$genreNom["GenreNom"].' <br/>
+          <h3>Synopsis : </h3><br/>
+          '.utf8_encode($infoLivre["LivreDesc"]).'<br>
+        </p>
+      </div>
 
-  <div class="divAchatLivre3">
-    <p>
-      <h3>Description du Produit :</h3>
-      Nombre de page : <br/>
-      Collection : <br/>
-      Editeur : <br/>
-      Auteur : <br/>
-      Langue : <br/>
-      Moyennes  des commentaires clients : <img id="imgmoyenne" src="../Images/vote.png" alt="vote"><br/>
+      <div id="divAchatLivre2">
+        <img id="imgAchatLivre1" src="../Images/Couvertures/'.$infoLivre["LivreCouv"].'" alt="Couverture du livre">
+      </div>
+      <br/>
 
-    </p>
-  </div>
+      <div id="prixal">'.$infoLivre["LivrePrix"].'€</div>
+      <a href="../livreFiles/'.$infoLivre["LivreFile"].'" download>Telecharger le fichier</a>
 
-  <div class="divAchatLivre4">
-    <p>
-      <h3>Avis : </h3>
-      user 1 : Super <br/>
-      user 2 : Super <br/>
-      user 3 : Super <br/>
-      user 4 : Super <br/>
-      user 5 : Super <br/>
-      user 6 : Super <br/>
-    </p>
-  </div>
+    </div>';
+  }
+ ?>
+
+
+
 
 <div id="footeral">
-<?php include '../fichier_inc/footer.inc.php'; ?>
+  <?php include '../fichier_inc/footer.inc.php'; ?>
 </div>
