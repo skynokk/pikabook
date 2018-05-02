cb97ab72a96a62ee7a1653dbd08092a5f13d9f82
Oznerologos <lorenzo.logos@ynov.com>
2018-05-02 22:06:34 +0200

    bug fix ajoutlivre

--- code/ajoutPikabook.php
+++ code/ajoutPikabook.php
@@ -1,8 +1,94 @@
-﻿
-<?php
-  include("fichier_inc/connexionBDD.inc.php");
-  include("fichier_inc/header.inc.php");
- ?>
+<?php include("fichier_inc/connexionBDD.inc.php") ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	    <title>Pik a Book</title>
+	    <link rel="stylesheet" href="pikabook.css">
+	</head>
+
+	<body>
+	<header>
+		<?php
+		if (!isset($_SESSION['Login'])) {
+
+			echo "<input type=\"button\" class=\"co\" value=\"Inscription\" onclick=\"window.location.href='connexion_inscription/connexioninscription.php';\" />
+				<form method=\"POST\" action='".$_SERVER['PHP_SELF']."' id=\"form\">
+				<input type=\"text\" id=\"pseudo\" placeholder=\"Pseudo\" name=\"pseudoRapide\" value=\"";
+				if (isset($_POST['coRapide'])) {
+				 	echo $_POST['pseudoRapide'];
+				 }
+				echo "\" />
+				<input type=\"password\" id=\"mdp\" name=\"mdpRapide\" placeholder=\"Mot de Passe\"/>
+			 	<input class=\"boutonco\" type=\"submit\" name=\"coRapide\" value=\"connexion\">
+				</form>
+				<br/><br/>";
+
+		if (isset($_POST['coRapide'])) {
+      	if (isset($_POST['pseudoRapide']) && isset($_POST['mdpRapide']) && $_POST['pseudoRapide']!== '' && $_POST['mdpRapide']!== '') {/*VERIFICATION DES FORMULAIRES*/
+
+        $rechercheLogMdpRapide = $pdo -> query('SELECT CliPseudo, CliMdp FROM client ');
+        while ($donnees = $rechercheLogMdpRapide -> fetch()) {/*On parcours la base de donnée pour trouver le pseudo correspondant*/
+          if ($_POST['pseudoRapide'] == $donnees['CliPseudo']) {
+            $cryptMdpConnRapide= $_POST['mdpRapide'];
+            $cryptSalt = 'Jetestlesaltdesfonctionscrypt';
+            $mdpCryptéConnRap= crypt($cryptMdpConnRapide, $cryptSalt);
+            if ($mdpCryptéConnRap == $donnees['CliMdp']) {/*Quand le pseudo est trouvé on regarde si le mot de passe est similaire*/
+              $_SESSION['Login']= $_POST['pseudoRapide'];/*La session est active*/
+              header("refresh:0");/*la page est rafraichie*/
+              break;
+            }
+            else {  echo "<p class='erreurRap'>Mauvais mot de passe pour ".$_POST['pseudoRapide']."</p>";}/*SI LE MOT DE PASSE EST MAUVAIS*/
+          }
+        }
+      }
+      else {
+        echo "<p class='erreurRap'>Veuillez remplir tous les formulaires.</p>";/*SI LES FORMULAIRES NE SONT PAS REMPLIS*/
+      }
+    }
+}
+	else {//si la session est active alors les commandes de l'utilisateur sont accessibles
+		echo "<p class='floatRight'>Bonjour à toi ". $_SESSION['Login'] . " !</p>";
+		echo '<div class="clear"></div>';
+		echo "<form action='".$_SERVER['PHP_SELF']."' method=\"POST\">
+		<input type=\"submit\" name=\"Deconnexion\" value=\"Déconnexion\" class='btnDeco'>";
+		if (isset($_POST['Deconnexion'])) {
+			session_destroy();
+			header ('Refresh: 0');
+		}
+			echo "<br/><br/>
+	<a href=\"panier/panier\"> <img src=\"Images/petit-panier.png\" class=\"panier\" alt=\"panier\"></a>
+	<a href=\"vos_commandes/voscommandes\"><button type=\"button\" class=\"commande\" name=\"button\">Mes commandes</button></a>
+	<br/><br/>";
+	}
+?>
+
+			<h1><a href="index"><img class="logo" src="Images/logo4_1.png" alt="logo"/></a></h1>
+
+			<div id="searchBar">
+				 <input class="champ" type="text" placeholder="Search..."/>
+				 <input class="bouton" type="button" value="&#128269; " />
+		 </div>
+
+		    <div class="page">
+				<div class="cadre">
+					<nav>
+						<li><a class="navigator" href="#">Présentation</a></li>
+						<li><a class="navigator" href="a_la_une/alaune">A la Une</a></li>
+						<li><a class="navigator" href="page_livre/pagelivre">Livres</a></li>
+						<li><a class="navigator" href="best_seller/bestseller">Les Best-Sellers</a></li>
+						<li><a class="navigator" href="derniers_ajouts/derniersajouts">Les derniers ajouts</a></li>
+						<li><a class="navigator" href="user/utilisateur">Mon compte</a></li>
+						<?php if (isset($_SESSION['Login'])) {
+							$statutAdminResult= $pdo->query('SELECT CliStatut FROM client WHERE Clipseudo="'.$_SESSION['Login'].'"');
+   							$statutAdmin = $statutAdminResult -> fetch(PDO::FETCH_ASSOC);
+							if ($statutAdmin['CliStatut'] == '1') {
+								echo "<li class='admin'><a class=\"navigator admin\" href=\"ajoutPikabook.php\">Ajout de livre</a></li>";
+							}
+						} ?>
+					</nav>
+		</div>
+	</header>
 
 <!-- Aide -->
 <!-- La fonction addslashes($string) permet d'echapper tout  caractère qui le requiert, ici il s'agit le plus souvent d'echapper les ' ou " des input qui font echouer les requêtes SQL -->
@@ -64,26 +150,27 @@
 <!-- Formulaire d'ajout d'un genre -->
 <section id="genre">
   <fieldset>
-    <legend>Ajouter un genre</legend>
+    <legend>Ajouter un Genre</legend>
     <form id="formGenre" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-      <label for="genreNom">Nom du genre : </label><br><input id="genreNom" type="text" name="genreNom"><br><br>
-      <label for="genreDesc">Description du genre : </label><br><textarea id="genreDesc" name="genreDesc" rows="8" cols="80"></textarea><br><br>
-      <input type="submit"  value="Ajouter !">
+      <label for="genreNom">Nom du genre :</label><br> <input id="genreNom" type="text" name="genreNom"><br><br>
+      <label for="genreDesc">Description du genre : </label> <br> <textarea id="genreDesc" name="genreDesc" rows="8" cols="80"></textarea><br><br>
+      <input type="submit" value="Ajouter !">
     </form>
     <?php
-      if (isset($_POST["genreNom"]) AND isset($_POST["genreDesc"])) {
-        if ($_POST["genreNom"] !="" AND $_POST["genreDesc"] !="") {
+      if (isset($_POST["genreNom"]) AND isset($_POST['genreDesc'])){
+        if ($_POST["genreNom"]!="" AND $_POST["genreDesc"]!="") {
           $genreNom = addslashes(utf8_decode($_POST["genreNom"]));
           $genreDesc = addslashes(utf8_decode($_POST["genreDesc"]));
           $result = $pdo->exec("INSERT INTO genre (GenreNom, GenreDesc) VALUES ('$genreNom', '$genreDesc')");
+          var_dump($genreNom);
+          var_dump($genreDesc);
           echo "Genre ajouté !!";
         }
         else {
-          echo "<p>Veuillez saisir un nom et une description !!</p>";
+          echo "<p>Veuillez saisir un nom de genre et une description !</p>";
         }
       }
      ?>
-
   </fieldset>
 </section>
 <!-- Fin du formulaire genre -->
@@ -197,4 +284,5 @@
 <!-- Fin du formulaire livre -->
 
 
-<?php include("fichier_inc/footer.inc.php"); ?>
+<footer><br/><br/><br/><br/>Copyright © 2018 <strong>Pikabook</strong>, Tous droits réservés, <a href='mention_legale_cookies_contact/mentions_legales.php' class='noDecoration'>Mentions Légales</a> & <a href='mention_legale_cookies_contact/cookies.php' class='noDecoration'> Cookies </a>, <a href='mention_legale_cookies_contact/contact.php' class='noDecoration'>Contact</a> </footer></body>
+</html>
