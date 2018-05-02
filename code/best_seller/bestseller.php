685480e66b0436d2cbdf4998b8ee2cc435dc3df0
unknown <stacy.perales@ynov.com>
2018-05-02 17:21:02 +0200

    bestsellers

--- code/best_seller/bestseller.php
+++ code/best_seller/bestseller.php
@@ -6,7 +6,7 @@ include '../fichier_inc/header.inc.php';
 
   <div class="ligne">
       <div class="divBestSeller"><img class="imageBestSeller" src="../Images/livre1.jpg" > </div>
-      <div class="texteBestSeller"> <h2>ESCLAVE DE DAECH</h2> </div><br><br><br>
+      <div class="texteBestSeller"> <h2>Harry potter à l'école des sorciers</h2> </div><br><br><br>
       <div class="resumBest"><p>Le jour de ses onze ans, Harry POTTER, un orphelin élevé par un oncle et une tante qui le détenstent, voit son
       existence boulversée. Un géant vient le chercher pour l'emmener à Poudlard, une école de sorcellerie! Voler, en balai,
     jeter des sorts, combattre les trolls: Harry se révèle être un sorcier doué.
@@ -17,8 +17,15 @@ include '../fichier_inc/header.inc.php';
   <div class="break"></div>
 
   <div class="ligne">
-      <div class="texteBestSeller2"> <h2>Les Revenants</h2> </div>
-      <div class="divBestSeller"><img class="imageBestSeller" src="../Images/livre2.jpg" > </div>
+      <div class="texteBestSeller2"> <h2>Puzzle</h2> </div><br>
+      <div class="resumBestGauche">
+        <p>Ilan et Chloé sont spécialistes des chasses au trésor. Longtemps, ils ont rêvé de participer au jeu ultime, celui dont on ne connaît que le nom : Paranoïa.
+Le jour venu, ils reçoivent la règle numéro 1 : Quoi qu'il arrive, rien de ce que vous allez vivre n'est la réalité. Il s'agit d'un jeu.
+Suivie, un peu plus tard, de la règle numéro 2 : L'un d'entre vous va mourir.
+Et quand les joueurs trouvent un premier cadavre, jeu et réalité commencent à se confondre.
+Paranoïa peut alors réellement commencer...<p>
+</div>
+    <div class="divBestSellers"><img class="imageBestSeller" src="../Images/livres1.jpg" > </div>
   </div>
 
   <div class="break"></div>
@@ -26,8 +33,13 @@ include '../fichier_inc/header.inc.php';
   <div class="ligne">
       <div class="divBestSeller"><img class="imageBestSeller" src="../Images/livre3.jpg" > </div>
       <div class="texteBestSeller"> <h2>Les Animaux fantastiques</h2><br></div>
+      <div class="resumBest">
+        <p>J. K. Rowling, créatrice de la mythique saga Harry Potter, nous invite à découvrir une nouvelle ère du Monde des Sorciers, au côté du jeune explorateur et magizoologiste
+           Norbert Dragonneau. Revivez le film écrit par J. K. Rowling avec le texte original de l'auteur : l'intégrale des dialogues, mais aussi l'action, le jeu des personnages, les
+           descriptions des décors, les mouvements de la caméra. Une aventure épique et fabuleuse, une grande histoire de magie et d'amitié.</p>
   </div>
 
+</div>
 </div>
 
 <?php include '../fichier_inc/footer.inc.php'; ?>
