7ab943d98f8b4059473564a16e9babd13a0ec7e4
Nicolas Gerard <nicolas.gerard@ynov.com>
2018-05-02 18:20:24 +0200

    alaune

--- code/a_la_une/alaune.php
+++ code/a_la_une/alaune.php
@@ -4,37 +4,21 @@
     <br>
         <h1>A la une</h1>
     <div class="actu">
-      <h3>Actualités</h3>
-      <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page
-        avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années
-        1500Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page
+      <h2>Nuit de la lecture 2018</h2>
+      <p>Vendredi 12 janvier, lors de la conférence de presse au ministère de la Culture présentant l’événement, Françoise Nyssen en a profité pour rappeler son engagement auprès des acteurs du livre en France, qu’ils soient auteurs, éditeurs, bibliothécaires ou libraires. Pour la fille du fondateur des éditions Actes Sud, Hubert Nyssen, « la promotion de la culture est un enjeu démocratique de premier plan ». D’importants projets sont selon elle en cours, notamment suite au dépôt par Erik Orsenna de son rapport sur les bibliothèques. La ministre de la Culture aura aussi eu à cœur d’insister, comme Daniel Pennac, sur l’importance de la gratuité de la lecture publique, s’opposant ainsi au projet de redevance demandée par la SCELF (Société civile des éditeurs de langue française) pour toute lecture publique en bibliothèque d’une œuvre publiée par un éditeur qu’elle défend. Voilà qui est, avant nouvelle péripétie, plutôt rassurant. Et promet de beaux moments de partage ce 20 janvier.
           </p>
     </div>
     <div class="lorem">
-      <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page
-        avant impression. Le Lorem Ipsum est </p>
+      <p>Phaedra Patrick, lauréate 2017<br>Après Nadia Hashimi et son roman La perle et la coquille, sacré en 2016 par le Prix des Lectrices, c’est au tour de Phaedra Patrick, écrivain anglaise, de recevoir cette récompense, grâce au vote de plusieurs milliers de lectrices, 5 672 pour être exact, qui ont fait leur choix parmi les nouveautés parues chez Milady littérature en 2016.</p>
     </div>
     <!--<br><br><br><br><br><br><br><br><br><br><br>-->
-    <div class="actu">
-      <h3>Actualités</h3>
-      <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page
-        avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années
-          </p>
-    </div>
     <div class="lorem">
-      <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page
-        avant impression. Le Lorem Ipsum est </p>
+      <p>Si vous n’avez pas encore lu Les fabuleuses tribulations d’Arthur Pepper, sachez que le roman vient tout juste de paraître en format poche chez Milady. D’ici là, voici son résumé : <br>Sans être un conformiste forcené, Arthur Pepper est anglais. Il puise donc un certain réconfort dans l’idée que chaque chose est à sa place et boit du thé à heure fixe. Quand il a rencontré Miriam, il a tout de suite su que c’était la bonne. Ils se sont mariés, ont eu des enfants. Lorsque sa femme meurt après quarante ans d’un vie sans histoire, Arthur se calfeutre chez lui pour échapper aux visites de sa voisine, championne du monde de la tourte, qui, faute de pouvoir le réconforter, s’est mis en tête de le nourrir. Le voilà condamné à vivre seul avec ses souvenirs et la plante verte dont sa femme a pris soin pendant des années. Ses proches lui conseillent d’aller de l’avant. Aller de l’avant ? Mais pour aller où bon sang ?</p>
     </div>
-    <div class="actu">
-      <h3>Actualités</h3>
-      <p>Le Lorem Ipsum est simplement du faux eergrhtjtrkuytrjrstnuebtniut <br>
-        <img src="../Images/placeholder.png" alt="place" class="holder">
-          </p>
+    <div class="actu2">
+      <h2>Quels ont été les livres les plus populaires de l’année 2016 ?</h2>
+        <img src="../Images/top.png" alt="place" class="holder">
 
-    </div>
-    <div class="lorem">
-      <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page
-        avant impression. Le Lorem Ipsum est </p>
     </div>
   </div>
 
