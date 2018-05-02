b1babc84a8a2f719bef299f4a31cf6171b4131d3
skynokk <jean.ferro@ynov.com>
2018-05-02 11:45:59 +0200

    cheminImages

--- code/best_seller/bestseller.php
+++ code/best_seller/bestseller.php
@@ -3,27 +3,26 @@ include '../fichier_inc/header.inc.php';
 ?>
 
 <div class="divPageBestSeller">
+  
   <div class="ligne">
       <div class="divBestSeller"><img class="imageBestSeller" src="../Images/livre1.jpg" > </div>
       <div class="texteBestSeller"> <h2>ESCLAVE DE DAECH</h2> </div>
   </div>
+
   <div class="break"></div>
 
   <div class="ligne">
       <div class="texteBestSeller2"> <h2>Les Revenants</h2> </div>
       <div class="divBestSeller"><img class="imageBestSeller" src="../Images/livre2.jpg" > </div>
   </div>
+
   <div class="break"></div>
+
   <div class="ligne">
       <div class="divBestSeller"><img class="imageBestSeller" src="../Images/livre3.jpg" > </div>
-      <div class="texteBestSeller"> <h2>Les Animaux fantastiques</h2><br><p><?php
-              $nom = $pdo->query('SELECT `CliNom` FROM `client`');
-              while ($row = $nom->fetch()) {
-                var_dump($row);
-                echo $row['CliNom'] . "\n";
-              }
-              ?></></p> </div>
+      <div class="texteBestSeller"> <h2>Les Animaux fantastiques</h2><br></div>
   </div>
+
 </div>
 
 <?php include '../fichier_inc/footer.inc.php'; ?>
